d93f27f66b74340bac6a59c78aee62741cd29ea0
gyuzero <gyuzero0103@gmail.com>
2023-08-19 15:15:04 +0900

    Catalogs 서비스에 Hystrix 적용

--- src/main/java/egovframework/msa/sample/service/impl/CustomerApiServiceImpl.java
+++ src/main/java/egovframework/msa/sample/service/impl/CustomerApiServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import egovframework.msa.sample.service.CustomerApiService;
 
 @Service
@@ -13,8 +15,15 @@ public class CustomerApiServiceImpl implements CustomerApiService {
 	private RestTemplate restTemplate;
 
 	@Override
+	@HystrixCommand(fallbackMethod = "getCustomerDetailFallback") // fallback 메소드는 customers 서비스가 에러 또는 지연될 경우 곧 바로 fallback 메소드를 호출하여 에러 전파를 방지한다.
 	public String getCustomerDetail(String customerId) {
-		return restTemplate.getForObject("http://localhost:8082/customers/" + customerId, String.class);
+		throw new RuntimeException("I/O Exception"); // customers 서비스에 강제로 Exception 발생
+		// return restTemplate.getForObject("http://localhost:8082/customers/" + customerId, String.class);
+	}
+	
+	public String getCustomerDetailFallback(String customerId, Throwable ex) {
+		System.out.println("Error:" + ex.getMessage());
+		return "고객정보 조회가 지연되고 있습니다.";
 	}
 
 }
